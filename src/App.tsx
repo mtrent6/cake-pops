65a6c58e850d909b7e9aed3e9bc1fbf0a3997e9a
Trenton Morrell <mtrent6@vt.edu>
2022-02-07 14:23:44 -0500

    fixing

--- src/App.tsx
+++ src/App.tsx
@@ -122,7 +122,7 @@ function App() {
             {/* @ts-ignore */}
             <ScrollPage page={3}>
               {/* @ts-ignore */}
-              <Animator animation={batch(Fade(), Sticky())}>
+              <Animator animation={MoveOut(-1000, 0)}>
                 <span style={{ fontSize: "40px" }}>
                   <p>We make custom cake pops for any event for YOU!</p>
                 </span>
