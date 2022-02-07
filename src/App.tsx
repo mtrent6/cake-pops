389b0e120531da8b0bdd1f63477026585bdcd1c8
Trenton Morrell <mtrent6@vt.edu>
2022-02-07 12:34:42 -0500

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,106 @@
+import "./App.css";
+import logo from "./assets/logo.jpg";
+import React, { useState } from "react";
+import SimpleImageSlider from "react-simple-image-slider";
+import cake1 from "./assets/cake1.jpeg";
+import cake2 from "./assets/cake2.jpeg";
+import cake3 from "./assets/cake3.jpeg";
+import cake4 from "./assets/cake4.jpeg";
+import cake5 from "./assets/cake5.jpeg";
+import cake6 from "./assets/cake6.jpeg";
+import cake7 from "./assets/cake7.jpeg";
+import cake8 from "./assets/cake8.jpeg";
+import cake9 from "./assets/cake9.jpeg";
+import cake10 from "./assets/cake10.jpeg";
+import TemporaryDrawer from "./Drawer";
+
+const images = [
+  { url: cake1 },
+  { url: cake2 },
+  { url: cake3 },
+  { url: cake4 },
+  { url: cake5 },
+  { url: cake6 },
+  { url: cake7 },
+  { url: cake8 },
+  { url: cake9 },
+  { url: cake10 },
+];
+
+
+
+
+
+function App() {
+
+  return (
+    <div>
+      <TemporaryDrawer/>
+      <div className="bg-ambar-50 flex flex-col h-screen text-center">
+        {/* <header className="font-bold">Alexandria Cake Pops</header> */}
+        <img
+          style={{
+            width: 200,
+            height: 100,
+            alignSelf: "center",
+            marginTop: 50,
+          }}
+          src={logo}
+        ></img>
+
+        <div style={{ alignSelf: "center", marginTop: 100 }}>
+          <SimpleImageSlider
+            width={400}
+            height={400}
+            images={images}
+            showBullets={false}
+            showNavs={true}
+            autoPlay={true}
+          />
+        </div>
+        <div
+          id="aboutUs"
+          style={{ marginLeft: 50, marginRight: 50, marginTop: 30 }}
+        >
+          <p>Share the LOVE of Cake Pops!</p>
+          <p>
+            Alexandria Cake Pop Company, also known as ACPC, started in January
+            2012 with one goal in mind: share the LOVE of cake pops!
+          </p>
+          <p>
+            Tamara Wilson, founder of ACPC, was introduced to cake pops by her
+            grandmother who was fascinated with these sweet concoctions which
+            were becoming extremely popular in Tamara's hometown of San Antonio,
+            Texas. Sharing a love for baking and inspired by the popular hit
+            series DC Cupcakes, Tamara took a leap of faith, quit her day job,
+            and started Alexandria Cake Pop Company in Alexandria, Virginia.
+          </p>
+          <p>
+            ACPC soon became renowned for its creative abilities and delicious
+            desserts. ACPC won the 2012 National Capital Area Cupcake Challenge
+            for creating Chocolate Peppermint Bliss as well as second-runner up
+            in the Adorable Cake Pop Contest hosted by Fox Broadcasting Company.
+          </p>
+          <p>
+            After working with ACPC on a fundraiser, Cerisse Zellmer of Fairfax,
+            Virginia was in the right place at the right time when ACPC became
+            available. Seeing its performance and potential, we echoed Tamara's
+            leap of faith; we jumped in. Our own decorative baking skills and
+            business experience are ready to bring you fun and creative cake
+            pops! Tell us your needs, or let us help you shape them. Expand your
+            dessert horizons with ACPC!
+          </p>
+          <p>
+            ​​We enjoy spreading the LOVE of cake pops and and we also donate to
+            multiple DC area chartities. Some of the charities include Cystic
+            Fibrosis Foundation, Give Back Alexandria, and Operation Second
+            Chance, a non-profit dedicated to serving the wounded and ill-combat
+            veterans.
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Drawer.tsx
@@ -0,0 +1,96 @@
+import * as React from "react";
+import Box from "@mui/material/Box";
+import Drawer from "@mui/material/Drawer";
+import Button from "@mui/material/Button";
+import List from "@mui/material/List";
+import Divider from "@mui/material/Divider";
+import ListItem from "@mui/material/ListItem";
+import ListItemIcon from "@mui/material/ListItemIcon";
+import ListItemText from "@mui/material/ListItemText";
+import InboxIcon from "@mui/icons-material/MoveToInbox";
+import MailIcon from "@mui/icons-material/Mail";
+import { useState } from "react";
+
+const HamburgerMenu = (props) => {
+  const { isOpen, setIsOpen } = props;
+  const genericHamburgerLine = `h-1 w-6 my-1 rounded-full bg-black transition ease transform duration-300`;
+  return (
+    <div style={{ position: "fixed", right: 0, zIndex: 1000 }}>
+      <button
+        className="flex flex-col h-12 w-12 justify-center items-center group"
+        onClick={() => setIsOpen(!isOpen)}
+      >
+        <div
+          className={`${genericHamburgerLine} ${
+            isOpen
+              ? "rotate-45 translate-y-3 opacity-50 group-hover:opacity-100"
+              : "opacity-50 group-hover:opacity-100"
+          }`}
+        />
+        <div
+          className={`${genericHamburgerLine} ${
+            isOpen ? "opacity-0" : "opacity-50 group-hover:opacity-100"
+          }`}
+        />
+        <div
+          className={`${genericHamburgerLine} ${
+            isOpen
+              ? "-rotate-45 -translate-y-3 opacity-50 group-hover:opacity-100"
+              : "opacity-50 group-hover:opacity-100"
+          }`}
+        />
+      </button>
+    </div>
+  );
+};
+
+export default function TemporaryDrawer(props) {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleDrawer = () => {
+      setIsOpen(false)
+  }
+
+  const list = (anchor) => (
+    <Box
+      sx={{ width: anchor === "top" || anchor === "bottom" ? "auto" : 250 }}
+      role="presentation"
+      onKeyDown={toggleDrawer}
+      
+    >
+      <List>
+        {["Inbox", "Starred", "Send email", "Drafts"].map((text, index) => (
+          <ListItem button key={text}>
+            <ListItemIcon>
+              {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+            </ListItemIcon>
+            <ListItemText primary={text} />
+          </ListItem>
+        ))}
+      </List>
+      <Divider />
+      <List>
+        {["All mail", "Trash", "Spam"].map((text, index) => (
+          <ListItem button key={text}>
+            <ListItemIcon>
+              {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+            </ListItemIcon>
+            <ListItemText primary={text} />
+          </ListItem>
+        ))}
+      </List>
+    </Box>
+  );
+
+  return (
+    <div>
+      <React.Fragment>
+        <HamburgerMenu isOpen={isOpen} setIsOpen={setIsOpen} />
+
+        <Drawer anchor={"right"} open={isOpen} onClose={toggleDrawer}>
+          {list("right")}
+        </Drawer>
+      </React.Fragment>
+    </div>
+  );
+}
