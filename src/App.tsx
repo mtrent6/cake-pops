6bd0825228484707488410f341abb9dcf3973cef
Trenton Morrell <trentkm@amazon.com>
2022-03-07 20:35:50 -0500

    more pages

--- src/App.tsx
+++ src/App.tsx
@@ -2,19 +2,19 @@ import "./App.css";
 import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { Order, Delivery } from './Order'
+import { Order, Delivery, Receipt } from './Order'
 import MasonryImageList from "./MasonryImage";
 import Home from './Home'
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/home" element={<Home />} />
-
+        <Route path="/" element={<Home />}/>
+        <Route path="/home" element={<Home />}/>
         <Route path="/order" element={<Order />} />
         <Route path="/gallery" element={<MasonryImageList />} />
         <Route path="/order/delivery" element={<Delivery />} />
+        <Route path="/order/receipt" element={<Receipt />} />
 
 
       </Routes>
--- src/Order.tsx
+++ src/Order.tsx
@@ -17,7 +17,35 @@ import OutlinedInput from "@mui/material/OutlinedInput";
 import Button from "@mui/material/Button";
 import { Outlet, Link } from "react-router-dom";
 
+function orderNumber() {
+  let now = Date.now().toString() // '1492341545873'
+  // pad with extra random digit
+  now += now + Math.floor(Math.random() * 10)
+  // format
+  return  [now.slice(0, 4), now.slice(4, 10), now.slice(10, 14)].join('-')
+}
 
+
+export const Receipt = () => {
+
+  return (
+    <>
+      <TemporaryDrawer />
+      <div className="bg-ambar-50"  >
+        <div style={{display: 'flex', justifyContent: 'center', paddingTop: 200, alignItems: 'center', flexDirection: 'column'}}>
+          <p style={{fontSize: 24}}>Thank you for your Order!</p>
+          <p style={{fontSize: 20}}>Your Order Number is: </p>
+          <p style={{fontSize: 20}}>{orderNumber()} </p>
+          <p style={{textAlign: 'center'}}>Please email us at email@gmail.com with any questions</p>
+
+
+          </div>
+        
+      </div>
+    </>
+  )
+
+}
 
 export const Delivery = () => {
   const [name, setName] = useState("");
@@ -127,7 +155,7 @@ export const Delivery = () => {
           </div>
         </Box>
         <div style={{ paddingTop: 30 }}>
-          <Link to={"/order/delivery"}>
+          <Link to={"/order/receipt"}>
             <Button variant="contained">Submit</Button>
           </Link>
         </div>
