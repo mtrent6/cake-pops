796b01b66f4d0f413122bb5179c3aac2a45b9546
Trenton Morrell <trentkm@amazon.com>
2022-03-07 21:09:26 -0500

    better workflows

--- src/MasonryImage.tsx
+++ src/MasonryImage.tsx
@@ -5,6 +5,8 @@ import ImageListItem from '@mui/material/ImageListItem';
 import TemporaryDrawer from './Drawer';
 import { CAKE_POP_PHOTOS } from './photos';
 import { useEffect } from 'react'
+import Button from "@mui/material/Button";
+import { Outlet, Link } from "react-router-dom";
 
 const photos = CAKE_POP_PHOTOS.map(pop => {
     return {
@@ -21,11 +23,11 @@ export default function MasonryImageList() {
 
 
     return (
-        <div>
+        <div style={{ height: window.screen.height }}>
             <TemporaryDrawer />
 
             <p style={{ textAlign: 'center', padding: 30 }}>Gallery</p>
-            <Box sx={{ width: window.screen.width, height: window.screen.height - 80, overflowY: 'scroll' }}>
+            <Box sx={{ width: window.screen.width, height: window.screen.height - 175, overflowY: 'auto' }}>
                 <ImageList variant="masonry" cols={2} gap={8}>
                     {photos.map((item, i) => (
                         <ImageListItem key={item.img}>
@@ -39,59 +41,12 @@ export default function MasonryImageList() {
                     ))}
                 </ImageList>
             </Box>
+            <div style={{ paddingTop: 20, display: 'flex', justifyContent: 'center' }}>
+                <Link to={"/order"}>
+                    <Button variant="contained">Order Now!</Button>
+                </Link>
+            </div>
         </div>
 
     );
 }
-
-const itemData = [
-    {
-        img: 'https://images.unsplash.com/photo-1549388604-817d15aa0110',
-        title: 'Bed',
-    },
-    {
-        img: 'https://images.unsplash.com/photo-1525097487452-6278ff080c31',
-        title: 'Books',
-    },
-    {
-        img: 'https://images.unsplash.com/photo-1523413651479-597eb2da0ad6',
-        title: 'Sink',
-    },
-    {
-        img: 'https://images.unsplash.com/photo-1563298723-dcfebaa392e3',
-        title: 'Kitchen',
-    },
-    {
-        img: 'https://images.unsplash.com/photo-1588436706487-9d55d73a39e3',
-        title: 'Blinds',
-    },
-    {
-        img: 'https://images.unsplash.com/photo-1574180045827-681f8a1a9622',
-        title: 'Chairs',
-    },
-    {
-        img: 'https://images.unsplash.com/photo-1530731141654-5993c3016c77',
-        title: 'Laptop',
-    },
-    {
-        img: 'https://images.unsplash.com/photo-1481277542470-605612bd2d61',
-        title: 'Doors',
-    },
-    {
-        img: 'https://images.unsplash.com/photo-1517487881594-2787fef5ebf7',
-        title: 'Coffee',
-    },
-    {
-        img: 'https://images.unsplash.com/photo-1516455207990-7a41ce80f7ee',
-        title: 'Storage',
-    },
-    {
-        img: 'https://images.unsplash.com/photo-1597262975002-c5c3b14bbd62',
-        title: 'Candle',
-    },
-    {
-        img: 'https://images.unsplash.com/photo-1519710164239-da123dc03ef4',
-        title: 'Coffee table',
-    },
-
-];
