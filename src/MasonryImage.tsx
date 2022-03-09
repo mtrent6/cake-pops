4e1c7018f258b91ab9a16e45f3b8dcc82b0f2925
Trenton Morrell <trentkm@amazon.com>
2022-03-09 18:01:17 -0500

    getting modals to work

--- src/MasonryImage.tsx
+++ src/MasonryImage.tsx
@@ -4,17 +4,32 @@ import ImageList from '@mui/material/ImageList';
 import ImageListItem from '@mui/material/ImageListItem';
 import TemporaryDrawer from './Drawer';
 import { CAKE_POP_PHOTOS } from './photos';
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 import Button from "@mui/material/Button";
 import { Outlet, Link } from "react-router-dom";
 import ImageListItemBar from '@mui/material/ImageListItemBar';
-import IconButton from '@mui/material/IconButton';
-import StarBorderIcon from '@mui/icons-material/StarBorder';
+
+import cake6 from "./assets/cake6.jpeg";
+import Typography from '@mui/material/Typography';
+import Modal from '@mui/material/Modal';
+
+const style = {
+    position: 'absolute',
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
+    width: 400,
+    bgcolor: 'background.paper',
+    border: '2px solid #000',
+    boxShadow: 24,
+    opacity: .9,
+    p: 4,
+};
 
 const photos = CAKE_POP_PHOTOS.map((pop, i) => {
     return {
         img: pop,
-        title: 'cake pop ' + i
+        title: 'cake pop $' + i
     }
 })
 
@@ -24,10 +39,38 @@ export default function MasonryImageList() {
 
     }, [])
 
+    const [isOpen, setIsOpen] = useState(false)
+
+    const handleShowDialog = () => {
+        setIsOpen(!isOpen);
+        console.log('cliked');
+    };
+
+
 
 
     return (
         <div style={{ height: window.screen.height }}>
+            <Modal
+                open={isOpen}
+                onClose={handleShowDialog}
+                aria-labelledby="modal-modal-title"
+                aria-describedby="modal-modal-description"
+            >
+                <Box sx={style}>
+                    <Typography id="modal-modal-title" variant="h6" component="h2">
+                        Here is a sample cake pop
+                    </Typography>
+                    <Typography id="modal-modal-description" sx={{ mt: 2 }}>
+                        Duis mollis, est non commodo luctus, nisi erat porttitor ligula.
+                    </Typography>
+                    <img
+                        src={cake6}
+                        alt={'modal image'}
+                    // loading="lazy"
+                    />
+                </Box>
+            </Modal>
             <TemporaryDrawer />
 
             <p style={{ textAlign: 'center', padding: 30 }}>Gallery</p>
@@ -36,10 +79,11 @@ export default function MasonryImageList() {
                     {photos.map((item, i) => (
                         <ImageListItem key={item.img}>
                             <img
+                                onClick={handleShowDialog}
                                 src={`${item.img}?w=248&fit=crop&auto=format`}
                                 srcSet={`${item.img}?w=248&fit=crop&auto=format&dpr=2 2x`}
                                 alt={'' + i}
-                                // loading="lazy"
+                            // loading="lazy"
                             />
                             <ImageListItemBar
                                 sx={{
@@ -49,15 +93,7 @@ export default function MasonryImageList() {
                                 }}
                                 title={item.title}
                                 position="top"
-                                actionIcon={
-                                    <IconButton
-                                        sx={{ color: 'white' }}
-                                        aria-label={`star ${item.title}`}
-                                    >
-                                        <StarBorderIcon />
-                                    </IconButton>
-                                }
-                                actionPosition="left"
+
                             />
                         </ImageListItem>
                     ))}
