9281eafa71cf526ed92170aba75a9245580d8fb7
Trenton Morrell <mtrent6@vt.edu>
2022-03-27 11:43:01 -0400

    better width

--- src/Order/Order.tsx
+++ src/Order/Order.tsx
@@ -113,7 +113,7 @@ export const Order = () => {
             </FormControl>
             <p style={{ paddingBottom: 8 }}>select the color for your cake pops</p>
             <ColorInput
-              sx={{ paddingLeft: 150, paddingRight: 150 }}
+              sx={{ paddingLeft: '11vh', paddingRight: '11vh' }}
               format="hex"
               withPicker={false}
               swatches={['#25262b', '#868e96', '#fa5252', '#e64980', '#be4bdb', '#7950f2', '#4c6ef5', '#228be6', '#15aabf', '#12b886', '#40c057', '#82c91e', '#fab005', '#fd7e14']}
