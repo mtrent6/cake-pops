b44d91c57fba97078e79a00fbcb8b62b808b2a17
Trenton Morrell <mtrent6@vt.edu>
2022-03-27 12:28:43 -0400

    lots of upgrades

--- src/Order/Order.tsx
+++ src/Order/Order.tsx
@@ -3,7 +3,6 @@ import Box from "@mui/material/Box";
 import TextField from "@mui/material/TextField";
 import AdapterDateFns from "@mui/lab/AdapterDateFns";
 import LocalizationProvider from "@mui/lab/LocalizationProvider";
-import DatePicker from "@mui/lab/DatePicker";
 import { useState } from "react";
 import InputLabel from "@mui/material/InputLabel";
 import MenuItem from "@mui/material/MenuItem";
@@ -15,13 +14,24 @@ import OutlinedInput from "@mui/material/OutlinedInput";
 import Button from "@mui/material/Button";
 import { Link } from "react-router-dom";
 import TemporaryDrawer from "../Drawer";
-import { RichTextEditor } from '@mantine/rte';
+import { MultiSelect, Blockquote } from '@mantine/core';
 import { ColorInput } from '@mantine/core';
+import { NumberInput } from '@mantine/core';
+import { Tabs } from '@mantine/core';
+import { Photo, MessageCircle, Settings } from 'tabler-icons-react';
+import { CheckIcon } from '@modulz/radix-icons';
+import { ColorSwatch, Group, useMantineTheme } from '@mantine/core';
+import { DatePicker } from '@mantine/dates';
 
 
 
 
+
+const mantineStyle = { paddingLeft: '5vh', paddingRight: '5vh', paddingTop: '1vh', paddingBottom: '1vh', fontFamily: 'monospace' }
 export const Order = () => {
+  const theme = useMantineTheme();
+  const [checked, setChecked] = useState(true);
+
 
   const [quantity, setQuantity] = useState(0);
 
@@ -73,107 +83,42 @@ export const Order = () => {
   return (
     <>
       <TemporaryDrawer />
-      <div className="bg-ambar-50 flex flex-col h-screen text-center">
-        <p style={{ fontSize: 14, marginBottom: 10 }}>
-          Create your cakepop order below!
-        </p>
-        <Box
-          component="form"
-          sx={{
-            "& .MuiTextField-root": { m: 1, width: "25ch" },
-          }}
-          noValidate
-          autoComplete="off"
-        >
-          <div>
-            <p style={{ paddingBottom: 5 }}>select two flavors</p>
-            <FormControl style={{ width: 250 }} error={flavor.length > 2}>
-              <InputLabel id="demo-simple-select-label"      >
-                Flavor
-              </InputLabel>
+      <Tabs sx={{ paddingTop: '1vh' }} color="green" tabPadding="xl">
+        <Tabs.Tab label="Info" icon={<Photo size={14} />}>
+          <Blockquote color="pink" cite="– Forrest Gump">
+            If eating cake is wrong I don't want to be right
+          </Blockquote></Tabs.Tab>
+        <Tabs.Tab label="Order" icon={<MessageCircle size={14} />}>    <div className="bg-ambar-50 flex flex-col h-screen text-center">
+          <p style={{ fontSize: 14, marginBottom: 10 }}>
+            Create your cakepop order below!
+          </p>
 
-              <Select
-                labelId="demo-simple-select-label"
-                id="demo-simple-select"
-                value={flavor}
-                label="Flavorrrrrrrr"
-                multiple
-                onChange={handleFlavorChange}
-                input={<OutlinedInput label="Flavor" />}
-                renderValue={(selected) => selected.join(" and ")}
-              >
-                {flavors.map((flv) => (
-                  <MenuItem key={flv} value={flv}>
-                    {/* @ts-ignore */}
-                    <Checkbox checked={flavor.indexOf(flv) > -1} />
-                    <ListItemText primary={flv} />
-                  </MenuItem>
-                ))}
-              </Select>
-            </FormControl>
-            <p style={{ paddingBottom: 8 }}>select the color for your cake pops</p>
+          <div>
+            <MultiSelect
+              sx={mantineStyle}
+              data={flavors}
+              label="Select two flavors"
+              placeholder="Pick all that you like"
+            />
+
             <ColorInput
-              sx={{ paddingLeft: '11vh', paddingRight: '11vh' }}
+              sx={mantineStyle}
               format="hex"
               withPicker={false}
+              label={"Select your colors"}
               swatches={['#25262b', '#868e96', '#fa5252', '#e64980', '#be4bdb', '#7950f2', '#4c6ef5', '#228be6', '#15aabf', '#12b886', '#40c057', '#82c91e', '#fab005', '#fd7e14']}
             />
-            {/* 
-            <FormControl style={{ width: 250 }} error={flavor.length > 2}>
-              <InputLabel id="demo-simple-select-label">Color</InputLabel>
-              <Select
-                labelId="demo-simple-select-label"
-                id="demo-simple-select"
-                value={color}
-                onChange={handleColorChange}
-                label="Color"
-                input={<OutlinedInput label="Color" />}
-                renderValue={(selected) => selected}
-
-              >
-                <MenuItem value="blue" style={{ backgroundColor: 'blue', color: 'white' }}>
-                  <ListItemText primary={'blue'} />
 
 
-                </MenuItem>
-                <MenuItem style={{ backgroundColor: 'magenta' }} value="magenta">
-                  <ListItemText primary={'magenta'} />
-
-                </MenuItem>
-                <MenuItem style={{ backgroundColor: 'yellow' }} value="yellow">
-                  <ListItemText primary={'yellow'} />
-
-                </MenuItem>
-                <MenuItem style={{ backgroundColor: 'red' }} value="red">
-                  <ListItemText primary={'red'} />
-                </MenuItem>
-                <MenuItem style={{ backgroundColor: 'green' }} value="green">
-                  <ListItemText primary={'green'} />
-                </MenuItem>
-              </Select>
-            </FormControl> */}
-
-
-            <p style={{ paddingBottom: 5 }}> enter how many cake pops you wnat</p>
-
-            <TextField
+            <NumberInput
+              sx={mantineStyle}
+              defaultValue={1}
+              placeholder="# of cakepops in dozens"
+              label="# of cakepops in dozens"
               required
-              id="outlined-required"
-              value={quantity}
-              inputProps={{ inputMode: "numeric" }}
-              onChange={handleQuantityChange}
-              label="Quantity"
             />
-            <p style={{ paddingBottom: 5 }}> select the date you wish to recieve them by</p>
 
-            <LocalizationProvider dateAdapter={AdapterDateFns}>
-              <DatePicker
-                label="Date You Need Cake Pops By"
-                value={date}
-                onChange={handleDateChange}
-                renderInput={(params) => <TextField {...params} />}
-              />
-            </LocalizationProvider>
+            <DatePicker sx={mantineStyle} placeholder="Pick date" label="Event date" required />
 
             <div style={{ marginTop: 80, marginBottom: 80 }}>
               <Link to={"/order/delivery"}>
@@ -181,9 +126,11 @@ export const Order = () => {
               </Link>
             </div>
 
+
           </div>
-        </Box>
-      </div>
+        </div ></Tabs.Tab>
+      </Tabs>
+
     </>
   );
 };
