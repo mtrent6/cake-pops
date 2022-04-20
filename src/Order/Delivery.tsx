dae20a12937d112ab49e3c1c44096b6919432f89
Trenton Morrell <trentkm@amazon.com>
2022-04-20 15:28:43 -0400

    order page almost done

--- src/Order/Delivery.tsx
+++ src/Order/Delivery.tsx
@@ -1,13 +1,14 @@
 import TemporaryDrawer from "../Drawer";
 import { useState } from 'react'
-
 import { Link } from "react-router-dom";
-import Button from "@mui/material/Button";
+// import Button from "@mui/material/Button";
+import { Box, TextInput, NumberInput, Button, Group } from '@mantine/core';
+
 import { connect } from "react-redux";
-import { TextInput } from '@mantine/core';
 import { Center } from '@mantine/core';
 import { Select } from '@mantine/core';
-import { states } from './helpers'
+import { states, formatPhoneNumber } from './helpers'
+import { useForm } from '@mantine/form';
 
 const mantineStyle = {
     fontFamily: 'monospace',
@@ -15,174 +16,112 @@ const mantineStyle = {
     paddingBottom: '1vh'
 }
 
-/**
- * taken from https://tomduffytech.com/how-to-format-phone-number-in-react/
- */
-function formatPhoneNumber(value) {
-    // if input value is falsy eg if the user deletes the input, then just return
-    if (!value) return value;
 
-    // clean the input for any non-digit values.
-    const phoneNumber = value.replace(/[^\d]/g, '');
+export const Delivery = (props) => {
 
-    // phoneNumberLength is used to know when to apply our formatting for the phone number
-    const phoneNumberLength = phoneNumber.length;
-
-    // we need to return the value with no formatting if its less then four digits
-    // this is to avoid weird behavior that occurs if you  format the area code to early
-
-    if (phoneNumberLength < 4) return phoneNumber;
-
-    // if phoneNumberLength is greater than 4 and less the 7 we start to return
-    // the formatted number
-    if (phoneNumberLength < 7) {
-        return `(${phoneNumber.slice(0, 3)}) ${phoneNumber.slice(3)}`;
-    }
-
-    // finally, if the phoneNumberLength is greater then seven, we add the last
-    // bit of formatting and return it.
-    return `(${phoneNumber.slice(0, 3)}) ${phoneNumber.slice(
-        3,
-        6
-    )}-${phoneNumber.slice(6, 10)}`;
-}
-
-
-export const Delivery = () => {
-    const [name, setName] = useState("");
-    const [email, setEmail] = useState("");
     const [number, setNumber] = useState("");
-    const [state, setState] = useState("");
-    const [city, setCity] = useState("");
-    const [zip, setZip] = useState("");
-    const [address, setAddress] = useState("");
-
-    const handleNameChange = (event) => {
-        setName(event.target.value);
-    };
-
-    const handleEmailChange = (event) => {
-        setEmail(event.target.value);
-    };
 
     const handleNumberChange = (event) => {
         const formattedPhoneNumber = formatPhoneNumber(event.target.value);
         setNumber(formattedPhoneNumber);
     };
 
-    const handleAddressChange = (event) => {
-        setAddress(event.target.value);
-    };
+    const email = (values) => {
+        console.log(props, values, number)
 
-    const handleStateChange = (event) => {
-        setState(event.target.value);
-    };
+    }
 
-    const handleCityChange = (event) => {
-        setCity(event.target.value);
-    };
+    const form = useForm({
+        initialValues: {
+            email: '',
+            name: '',
+            state: '',
+            city: '',
+            zip: '',
+            address: '',
+        },
 
-    const handleZipChange = (event) => {
-        setZip(event.target.value);
-    };
-
-    //       /**
-    //    * NOTE TO FUTURE ME, I SHOULD USE https://mantine.dev/form/use-form/ TO DO 
-    //    * THE FORM FOR THIS
-    //    * STATEFUL METHODS IS STUPID
-    //    * USE MANTINE
-    //    * GODSPEED
-    //    */
-
-    //   const form = useForm({
-    //     initialValues: {
-    //       email: '',
-    //       termsOfService: false,
-    //     },
-
-    //     validate: {
-    //       email: (value) => (/^\S+@\S+$/.test(value) ? null : 'Invalid email'),
-    //     },
-    //   });
+        validate: {
+            email: (value) => (/^\S+@\S+$/.test(value) ? null : 'Invalid email'),
+            zip: (value) => value.length === 5 ? null : 'Invalid zip code'
+        },
+    });
 
 
 
     return (
         <>
             <TemporaryDrawer />
+
             <Center>
+                <form onSubmit={form.onSubmit((values) => email(values))}>
+                    <div className="bg-ambar-50 flex flex-col h-screen text-center">
+                        <p style={{ fontSize: 14, marginBottom: 10, marginTop: 5 }}>
+                            Enter your delivery details below
+                        </p>
+                        <TextInput
+                            sx={mantineStyle}
+                            placeholder="John Doe"
+                            label="Full name"
+                            required
+                            {...form.getInputProps('name')}
+                        />
+                        <TextInput
+                            sx={mantineStyle}
+                            placeholder="(555) 555-5555"
+                            label="Phone number"
+                            inputMode="numeric"
+                            required
+                            value={number}
+                            onChange={handleNumberChange}
+                        />
+                        <TextInput
+                            sx={mantineStyle}
+                            placeholder="xyz@gmail.com"
+                            label="Email Address"
+                            required
+                            {...form.getInputProps('email')}
+                        />
+                        <TextInput
+                            sx={mantineStyle}
+                            placeholder="123 N Street"
+                            label="Street Address"
+                            required
+                            {...form.getInputProps('address')}
+                        />
+                        <TextInput
+                            sx={mantineStyle}
+                            placeholder="Richmond"
+                            label="City"
+                            required
+                            {...form.getInputProps('city')}
+                        />
+                        <Select
+                            label="State"
+                            placeholder="VA"
+                            searchable
+                            required
+                            nothingFound="N/A"
+                            data={states}
+                            {...form.getInputProps('state')}
+                        />
+                        <TextInput
+                            sx={mantineStyle}
+                            placeholder="23173"
+                            label="ZIP code"
+                            inputMode="numeric"
+                            required
+                            {...form.getInputProps('zip')}
+                        />
+                        <div style={{ paddingTop: 30 }}>
 
-                <div className="bg-ambar-50 flex flex-col h-screen text-center">
-                    <p style={{ fontSize: 14, marginBottom: 10, marginTop: 5 }}>
-                        Enter your delivery details below
-                    </p>
-                    <TextInput
-                        sx={mantineStyle}
-                        placeholder="John Doe"
-                        label="Full name"
-                        required
-                        value={name}
-                        onChange={handleNameChange}
-                    />
-                    <TextInput
-                        sx={mantineStyle}
-                        placeholder="(555) 555-5555"
-                        label="Phone number"
-                        inputMode="numeric"
-                        required
-                        value={number}
-                        onChange={handleNumberChange}
-                    />
-                    <TextInput
-                        sx={mantineStyle}
-                        placeholder="xyz@gmail.com"
-                        label="Email Address"
-                        required
-                        value={email}
-                        onChange={handleEmailChange}
-                    />
-                    <TextInput
-                        sx={mantineStyle}
-                        placeholder="123 N Street"
-                        label="Street Address"
-                        required
-                        value={address}
-                        onChange={handleAddressChange}
-                    />
-                    <TextInput
-                        sx={mantineStyle}
-                        placeholder="Richmond"
-                        label="City"
-                        required
-                        value={city}
-                        onChange={handleCityChange}
-                    />
-                    <Select
-                        label="State"
-                        placeholder="VA"
-                        searchable
-                        required
-                        nothingFound="N/A"
-                        data={states}
-                        value={state}
-                        onChange={handleStateChange}
-                    />
-                    <TextInput
-                        sx={mantineStyle}
-                        placeholder="23173"
-                        label="ZIP code"
-                        inputMode="numeric"
-                        required
-                        value={zip}
-                        onChange={handleZipChange}
-                    />
-                    <div style={{ paddingTop: 30 }}>
-                        <Link to={"/order/receipt"}>
-                            <Button sx={{ backgroundColor: '#FAD900', fontFamily: 'monospace', color: 'black' }} variant="contained">Submit</Button>
-                        </Link>
+                            <Button sx={{
+                                backgroundColor: '#228be6 !important',
+                                backgroundImage: '#228be6 !important'
+                            }} type="submit">Submit</Button>
+                        </div>
                     </div>
-
-                </div>
+                </form>
             </Center>
         </>
     )
--- src/Order/helpers.ts
+++ src/Order/helpers.ts
@@ -1,2 +1,35 @@
 export const states = ["AK", "AL", "AR", "AS", "AZ", "CA", "CO", "CT", "DC", "DE", "FL", "GA", "GU", "HI", "IA", "ID", "IL", "IN", "KS", "KY", "LA", "MA", "MD", "ME", "MI", "MN", "MO", "MS", "MT", "NC", "ND", "NE", "NH", "NJ", "NM", "NV", "NY", "OH", "OK", "OR", "PA", "PR", "RI", "SC", "SD", "TN", "TX", "UT", "VA", "VI", "VT", "WA", "WI", "WV", "WY"]
 
+
+/**
+ * taken from https://tomduffytech.com/how-to-format-phone-number-in-react/
+ */
+ export function formatPhoneNumber(value) {
+    // if input value is falsy eg if the user deletes the input, then just return
+    if (!value) return value;
+
+    // clean the input for any non-digit values.
+    const phoneNumber = value.replace(/[^\d]/g, '');
+
+    // phoneNumberLength is used to know when to apply our formatting for the phone number
+    const phoneNumberLength = phoneNumber.length;
+
+    // we need to return the value with no formatting if its less then four digits
+    // this is to avoid weird behavior that occurs if you  format the area code to early
+
+    if (phoneNumberLength < 4) return phoneNumber;
+
+    // if phoneNumberLength is greater than 4 and less the 7 we start to return
+    // the formatted number
+    if (phoneNumberLength < 7) {
+        return `(${phoneNumber.slice(0, 3)}) ${phoneNumber.slice(3)}`;
+    }
+
+    // finally, if the phoneNumberLength is greater then seven, we add the last
+    // bit of formatting and return it.
+    return `(${phoneNumber.slice(0, 3)}) ${phoneNumber.slice(
+        3,
+        6
+    )}-${phoneNumber.slice(6, 10)}`;
+}
+
