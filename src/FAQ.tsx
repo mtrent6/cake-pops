bc37eef3650dde5d92618970c57e3a1cd4271132
mtrent6 <mtrent6@vt.edu>
2022-12-26 15:10:00 -0500

    updating faq

--- src/FAQ.tsx
+++ src/FAQ.tsx
@@ -12,44 +12,37 @@ export const FAQ = () => {
         <div style={{ marginLeft: 20, marginRight: 10 }}>
 
             <p style={{ fontWeight: 800, padding: 0 }}>What are cake pops?</p>
-            <p style={{ padding: 0 }}>Cake pops are 3-4 bites of deliciously moist cake coated in only the finest chocolate and scrumptious toppings.
-                The average cake pop is the size of a golf ball, though the character and holiday pops are usually larger.</p>
+            <p style={{ padding: 0 }}>Cake pops are 2-3 bites of deliciously moist cake coated in the finest chocolate and scrumptious toppings. The average cake pop is the size of a golf ball.</p>
+
             <p style={{ fontWeight: 800 }}> How long do they last?</p>
-            <p style={{ padding: 0 }}> Our cake pops last 3 days at room temperature. They can be refrigerated for a week and they can be frozen for up to 3 weeks!
-                Just let them defrost until they reach room temperature and enjoy!</p>
+            <p style={{ padding: 0 }}>Our cake pops can be refrigerated for 7-10 days frozen for up to 3 weeks! Just let them defrost until they reach room temperature and enjoy!</p>
+
             <p style={{ fontWeight: 800 }}> How are the cake pops packaged?</p>
-            <p style={{ padding: 0 }}> Each cake pop is also individually wrapped for maximum freshness.
-                Pops must be stored in a dry & cool place, just like chocolate.</p>
+            <p style={{ padding: 0 }}>Each cake pop is individually wrapped for maximum freshness. Pops must be stored in a dry & cool place, just like chocolate.</p>
+
             <p style={{ fontWeight: 800 }}>Do your cake pops contain nuts or nut byproducts? Are they gluten-free?</p>
-            <p style={{ padding: 0 }}> Unfortunately, our cake pops are not produced in a nut-free facility, so we cannot guarantee cross contamination with nut byproducts.
-                In the future we hope to provide gluten-free and nut-free cake pops to accomodate certain dietary needs.</p>
+            <p style={{ padding: 0 }}>Our cake pops are not produced in a nut-free facility (we lease a shared kitchen space), so we cannot guarantee cross contamination with nut byproducts. We can create gluten-free chocolate cake pops.</p>
+
             <p style={{ fontWeight: 800 }}>When should I place my order?</p>
             <p style={{ padding: 0 }}>
-                For orders of 1-6 dozen, we kindly ask that you submit and confirm your order
-                5 business days prior to delivery date.
-                For orders of 6 dozen+ we require 7 business days notice.
-                Rush orders will incur a 15% surcharge.</p>
+                For orders of 1-6 dozen, we kindly ask that you submit and confirm your order 1-2 weeks prior to delivery date. For orders of over 6 dozen, we require 2 weeks or more notice. Rush orders will incur a 15% surcharge of the order total.
+            </p>
+
             <p style={{ fontWeight: 800 }}>How many cake pops should I order for
                 my event?</p>
-            <p style={{ padding: 0 }}>We usually recommend 1-2 cake pops per person for any event. Many of our regular clients have mentioned that the cake pops are always a hit and tend to order 2 per person.
-                What can we say... 1 cake pop is never enough!</p>
+            <p style={{ padding: 0 }}>We usually recommend 1-2 cake pops per person for any event. Many of our regular clients have mentioned that the cake pops are always a hit and tend to order 2 per person. What can we say... 1 cake pop is never enough!</p>
+
             <p style={{ fontWeight: 800 }}>Do I get a discount for large orders?</p>
-            <p style={{ padding: 0 }}>Yes! For every order of 11 dozen cake pops, you will receive one free dozen!</p>
+            <p style={{ padding: 0 }}>Yes! For every order of 12 dozen cake pops, you will receive one free dozen!
+            </p>
 
             <p style={{ fontWeight: 800 }}>Where do I pick up my cake pops? Can they be shipped or delivered?</p>
+            <p style={{ padding: 0 }}>Alexandria Cake Pop Company offers national shipping, local DMV delivery (rates based on zip code), and local no-charge pick up in Fairfax. During the warmer months of May through September, we ship on ice and insulated cooler bags.
+                ACPC will not be held responsible for any damages incurred during the shipping
+                process.
+            </p>
 
-            <p style={{ padding: 0 }}>Alexandria Cake Pop Company is an online retailer. We offer shipping cross the United States (excluding Hawaii and Alaska).
-                During the warmer months of May through September, we include thermo-technology insulation with ice packs.
-                The ice packs will increase the probability that the cake pops remain in a suitable condition.
-
-
-                ACPC will not be held responsible for any damages incurred during shipping process.
-
-
-
-                Time permitting, a Fairfax local meet-up can be arranged.</p>
             <p style={{ fontWeight: 800 }}>Do you have a cancelation policy?</p>
-
             <p style={{ padding: 0, paddingBottom: 20 }}> We require 72 hours notice prior to the expected arrival date of your order. There are no refunds on deposits of orders. No order can be refunded, exchanged or returned once it has been shipped or delivered by Alexandria Cake Pop Company.</p>
         </div></>
 }
